bb962b9770c4c5973758acb0f23e42eb89e2975a
Phil Monroe <phil@philmonroe.com>
2014-04-09 00:31:36 -0700

    cleanup

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -1,10 +1,15 @@
 class ReposController < ApplicationController
   def index
-    @repos = params[:repos].split(?,).uniq.map{ |name| Repo.find(current_user, name) }
+    names = params[:repos].split(?,).uniq
+
+    @repos = names.map { |name| Repo.find(current_user, name) }
+
   end
 
 
   def show
     @repo = Repo.find(current_user, "#{params[:user]}/#{params[:repo]}")
   end
+
+
 end
