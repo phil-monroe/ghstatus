5506934f14adf2cbf6bfb9b99232bf1d45559cb3
Phil Monroe <phil@philmonroe.com>
2014-04-09 00:31:53 -0700

    faster and works with public repos!

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -1,16 +1,25 @@
-Repo = Struct.new(:name, :pulls, :statuses) do
+::Repo = Struct.new(:name, :pulls, :statuses) do
   def self.find(current_user, name)
     pulls = current_user.github.pull_requests(name)
 
     statuses = Hash.new
     statuses['master'] = current_user.github.status(name, 'master', accept: 'application/vnd.github.she-hulk-preview+json')
 
-    pulls.inject(statuses) do |h, pull|
-      head = pull.head
-      status = current_user.github.status(name, head.ref, accept: 'application/vnd.github.she-hulk-preview+json')
-      h[head.ref] = status
-      h
+    threads = []
+    pulls.each do |pull|
+      threads << Thread.new do
+        begin
+          head = pull.head
+          # puts head.to_yaml
+          status = current_user.github.status(name, head.sha, accept: 'application/vnd.github.she-hulk-preview+json')
+          statuses[head.ref] = status
+        rescue => e
+          Rails.logger.error e.message
+          Rails.logger.error e.backtrace
+        end
+      end
     end
+    threads.map(&:join)
 
     Repo.new name, pulls, statuses
   end
